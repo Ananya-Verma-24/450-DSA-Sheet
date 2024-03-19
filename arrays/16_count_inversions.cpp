12b84d6283099394f96bfc2fcf0d18b9cf88113b
Ananya Verma <130167163+Ananya-Verma-24@users.noreply.github.com>
2024-03-19 17:33:25 +0530

    Create 16_count_inversions.cpp

---
+++ arrays/16_count_inversions.cpp
@@ -0,0 +1,98 @@
+/*
+Given an array of integers. Find the Inversion Count in the array. 
+
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
+If an array is sorted in the reverse order then the inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+ 
+
+Example 1:
+
+Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 
+has three inversions (2, 1), (4, 1), (4, 3).
+Example 2:
+
+Input: N = 5
+arr[] = {2, 3, 4, 5, 6}
+Output: 0
+Explanation: As the sequence is already 
+sorted so there is no inversion count.
+Example 3:
+
+Input: N = 3, arr[] = {10, 10, 10}
+Output: 0
+Explanation: As all the elements of array 
+are same, so there is no inversion count.
+Your Task:
+
+You don't need to read input or print anything.
+Your task is to complete the function inversionCount() which takes the array arr[] and the size of the array as inputs and returns the inversion count of the given array.
+
+Expected Time Complexity: O(NLogN).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 5*105
+1 ≤ arr[i] ≤ 1018
+*/
+
+// the main catch is to perform merge sort
+// and whenever left > right increase count to cnt = mid-left+1
+
+long long int cnt = 0;
+    
+    void merge(long long *arr, int low, int mid, int high) {
+    vector<long long> temp; // temporary array
+    long left = low;      // starting index of left half of arr
+    long right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        // right is smaller so we will check here
+        else {
+            temp.push_back(arr[right]);
+            cnt += mid-left+1; // *********************************************** this is the only change in merge sort************
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(long long *arr, int low, int high) {
+    if (low >= high) return;
+    long long mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);  // merging sorted halves
+}
+
+    long long int inversionCount(long long arr[], long long N)
+    {
+        // Your Code Here
+        mergeSort(arr, 0, N-1);
+        return cnt;
+    }
